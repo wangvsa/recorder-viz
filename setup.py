dfcade0face59827b113635eaf1e729400a48bc0
Chen Wang <wangvsa@gmail.com>
2024-06-14 14:30:31 -0700

    Ship a recorder-report binary
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="recorder-viz",                # Package name, e.g., pip install recorder-viz
-    version="0.5.2",
+    version="0.5.3",
     author="Chen Wang",
     author_email="wangvsa@gmail.com",
     description="Utilities for processing Recorder traces",
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/wangvsa/recorder-viz",
     packages=['recorder_viz'],                  # package for import: after installaion, import recorder_viz
     #package_data = {'recorder_viz': ['*.h']},   # *.h by default will not be copied, we use this to ship it.
+    scripts=['bin/recorder-report'],
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: University of Illinois/NCSA Open Source License",
